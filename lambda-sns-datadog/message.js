505f2e98d11ef80f514a5be66568a6983e3e2619
jerrettlevensalor-wf <jerrett.levensalor@workiva.com>
2016-02-01 15:22:41 -0700

    Adding message example and an example.json

---
+++ lambda-sns-datadog/message.js
@@ -0,0 +1,6 @@
+{
+  "default": "My kinesis-lambda-dd-sns-test",
+  "title": "Lambda Message Test",
+  "message": "The one and only sns test version 3 by Jerrett",
+  "url": "http://www.workiva.com/"
+}
